800838788716e4551125222d07adc40d90335119
Manon Pilaud <mandyypilaud@gmail.com>
2019-04-13 17:31:08 -0400

    styling

--- src/Location.js
+++ src/Location.js
@@ -2,35 +2,22 @@ import React from 'react'
 import {Button} from 'react-bootstrap'
 
 export default class Location extends React.Component{
-  state={
-    mapquest: ""
-  }
-  componentDidMount(){
-    fetch(`http://www.mapquestapi.com/geocoding/v1/reverse?key=${process.env.REACT_APP_MAPQUEST_API_KEY}&location=30.333472,-81.470448&includeRoadMetadata=true&includeNearestIntersection=true`)
-    .then(res=>res.json())
-    .then(data=>
-      this.setState({
-        mapquest: data.results[0].locations[0]
 
-      })
-    )
-
-
-  }
   render(){
     let {name,description,area,category,country,image,latitude,longitude,rating} = this.props.location
-    return(
+    console.log(latitude,longitude)
+    return !this.props.location?null:(
       <div>
           <center>
           <div>
             <h1>{name}</h1>
             <div id="side-info">
-              <h6>{area}</h6>
-              <h6>{category}</h6>
-              <h6>{country}</h6>
-              <img src={this.state.mapquest.mapUrl}/>
+              <h6>area: {area}</h6>
+              <h6>Park type: {category}</h6>
+              <h6>Country: {country}</h6>
+              <img src={`https://www.mapquestapi.com/staticmap/v5/map?key=${process.env.REACT_APP_MAPQUEST_API_KEY}&center=${latitude},${longitude}$zoom=0&size=200,200`}/>
             </div>
-            <img  id="location-image" src={image}/>
+            <img height="600" width="800"id="location-image" src={image}/>
             <br/>
             <button type="button" className="btn btn-outline-light">Visit Location</button>
         </div>
